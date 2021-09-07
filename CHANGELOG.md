788069d03315d59b2e1c2a7c2068f7971def0a13
Andrew Martin <andrew.thaddeus@gmail.com>
2021-09-07 14:08:54 -0400

    Prepare for 0.2.0.0 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,12 @@
 # Revision history for json-query
 
-## 0.1.1.0 -- ???
+## 0.2.0.0 -- 2021-09-07
 
 * Add `Monad` implementation for `MemberParser`.
 * Add `Alternative` implementation for `Parser` and `MemberParser`.
+* Add `Arrow` interface.
+* Add textual descriptions to all errors.
+* Track all errors in all choice-like typeclasses.
 
 ## 0.1.0.0 -- 2021-03-22
 
--- src/Json/Error.hs
+++ src/Json/Error.hs
@@ -13,20 +13,15 @@ module Json.Error
   ) where
 
 import Data.Bytes.Builder (Builder)
-import Data.Primitive (SmallArray)
 import Data.Text.Short (ShortText)
 import Json.Context (Context(..))
 import Data.ByteString.Short.Internal (ShortByteString(SBS))
-import Control.Monad.ST (runST)
 
 import qualified Data.Bytes.Builder as Builder
 import qualified Data.Bytes.Chunks as ByteChunks
 import qualified Data.Primitive as PM
-import qualified Data.Primitive.Contiguous as Arr
 import qualified Data.Text.Short.Unsafe as TS
-import qualified GHC.Exts as Exts
 import qualified Json.Context as Context
-import qualified Json.Path as Path
 
 -- | A single error message.
 data Error = Error
