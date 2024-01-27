451dcd8311d14a7730d462b10c301aaa64d4ddce
Brian McKeon <bmckeon@layer3com.com>
2024-01-27 11:48:19 -0500

    Fixes to make cabal check happy. Also added more checks to release script.

--- .github/check-release.sh
+++ .github/check-release.sh
@@ -16,7 +16,7 @@ die () {
     exit 1
 }
 
-
+# Main
 [ "$#" -eq 2 ] || die "Specify release version number and maintainer's email address."
 
 RELEASE_VERSION="$1"
@@ -29,7 +29,7 @@ CABAL_VERSION=$(awk '$1=="version:"{print $2}' "${CABAL_FILE}")
 
 if [ -z ${CABAL_VERSION} ];
 then
-  die "Couldn't parse version number from ${CABAL_FILE}"
+  die "Couldn't find version number in ${CABAL_FILE}"
 fi
 
 CABAL_VERSION_VALID=true
@@ -44,7 +44,7 @@ CABAL_GIT_URL=$(awk '$1=="bug-reports:"{print $2}' "${CABAL_FILE}")
 
 if [ -z ${CABAL_GIT_URL} ];
 then
-  die "Couldn't parse bug-reports field from ${CABAL_FILE}"
+  die "Couldn't find bug-reports field in ${CABAL_FILE}"
 fi
 
 REPO_GIT_URL=$(git config --get remote.origin.url)
@@ -57,12 +57,27 @@ then
   echo "You need to change the bug-reports field in ${CABAL_FILE} to ${EXPECTED_GIT_URL}"
 fi
 
+# Ensure homepage field exists in cabal file and is correct.
+CABAL_HOMEPAGE=$(awk '$1=="homepage:"{print $2}' "${CABAL_FILE}")
+
+if [ -z ${CABAL_HOMEPAGE} ];
+then
+  die "Couldn't find homepage field in ${CABAL_FILE}"
+fi
+
+CABAL_HOMEPAGE_VALID=true
+if [ ${CABAL_HOMEPAGE} != ${EXPECTED_GIT_URL} ];
+then
+  CABAL_HOMEPAGE_VALID=false
+  echo "You need to change the homepage field in ${CABAL_FILE} to ${EXPECTED_GIT_URL}"
+fi
+
 # Ensure maintainer email address is correct in cabal file.
 CABAL_EMAIL=$(awk '$1=="maintainer:"{print $2}' "${CABAL_FILE}")
 
 if [ -z ${CABAL_EMAIL} ];
 then
-  die "Couldn't parse maintainer field from ${CABAL_FILE}"
+  die "Couldn't find maintainer field in ${CABAL_FILE}"
 fi
 
 CABAL_EMAIL_VALID=true
@@ -70,6 +85,25 @@ if [ ${CABAL_EMAIL} != ${MAINTAINER_EMAIL} ];
 then
   CABAL_EMAIL_VALID=false
   echo "You need to change the maintainer field in ${CABAL_FILE} to ${MAINTAINER_EMAIL}"
+fi
+
+# Ensure source-repository entry exists and is correct.
+EXPECTED_SOURCE_REPOSITORY_LOCATION="${EXPECTED_GIT_URL#https:\/\/}"
+CABAL_SOURCE_REPOSITORY=$(awk '$1=="source-repository"{print $1}' "${CABAL_FILE}")
+
+if [ -z ${CABAL_SOURCE_REPOSITORY} ];
+then
+  die "Couldn't find source-repository field in ${CABAL_FILE}"
+fi
+
+FULL_GIT_URL="git://${EXPECTED_SOURCE_REPOSITORY_LOCATION}.git"
+CABAL_SOURCE_REPOSITORY_LOCATION=$(awk '$1=="location:"{print $2}' "${CABAL_FILE}")
+
+CABAL_SOURCE_REPOSITORY_VALID=true
+if [ ${CABAL_SOURCE_REPOSITORY_LOCATION} != "${FULL_GIT_URL}" ];
+then
+  CABAL_SOURCE_REPOSITORY_VALID=false
+  echo "You need to change the source-repository's location in ${CABAL_FILE} to ${FULL_GIT_URL}"
 fi
 
 # Ensure changelog contains an entry for this release version.
@@ -85,7 +119,9 @@ fi
 CHECKS=(
   "$CABAL_VERSION_VALID"
   "$CABAL_GIT_URL_VALID"
+  "$CABAL_HOMEPAGE_VALID"
   "$CABAL_EMAIL_VALID"
+  "$CABAL_SOURCE_REPOSITORY_VALID"
   "$CHANGELOG_VERSION_VALID"
 )
 
